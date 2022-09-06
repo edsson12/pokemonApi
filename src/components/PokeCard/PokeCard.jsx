7013ec9cb6d262c5024cb689724bf1841cfa3184
AnzickMedia <87082069+AnzickMedia@users.noreply.github.com>
2022-09-06 13:51:26 -0500

    agregando toggles y estilizando pokecard

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import "./PokeCard.scss";
 
-
 const PokeCard = ({ nombre, sprites, abilities, type, moves }) => {
-
-    const [toggleState, setToggleState] = useState(1);
-    const toggleTab = (index) => {
-        setToggleState(index);
-      };
+  const [toggleState, setToggleState] = useState(1);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <div className="o-card">
       <div className="o-imgcont">
@@ -24,38 +22,38 @@ const PokeCard = ({ nombre, sprites, abilities, type, moves }) => {
         <h2>{nombre}</h2>
       </div>
 
-
       <div className="o-tabs">
         <button
           className={toggleState === 1 ? "o-active" : "o-tab"}
           onClick={() => toggleTab(1)}
         >
-          Tab 1
+          Abilities
         </button>
         <button
           className={toggleState === 2 ? "o-active" : "o-tab"}
           onClick={() => toggleTab(2)}
         >
-          Tab 2
+          Moves
         </button>
-       
       </div>
 
-<div className={toggleState === 1 ? "o-abilities" : "o-abilities-inactive"}>
-      <h3>habilidades:</h3>
-      {abilities.map((a, key) => (
-        <div key={key}>
-          <p>{a.ability.name}</p>
-        </div>
-      ))}
+      <div
+        className={toggleState === 1 ? "o-abilities" : "o-abilities-inactive"}
+      >
+        
+        {abilities.map((a, key) => (
+          <div key={key}>
+            <p>{a.ability.name}</p>
+          </div>
+        ))}
       </div>
       <div className={toggleState === 2 ? "o-moves" : "o-moves-inactive"}>
-      <h3>moves:</h3>
-      {moves.map((m, key) => (
-        <div key={key}>
-          <p>{m.move.name}</p>
-        </div>
-      ))}
+        
+        {moves.map((m, key) => (
+          <div key={key}>
+            <p>{m.move.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
